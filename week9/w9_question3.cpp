7cb8238a61c0f97fe288c54fdbcaa696bc4f4a8a
KUMARI NEHA <92735311+200221192@users.noreply.github.com>
2022-06-08 00:35:54 +0530

    Add files via upload

---
+++ week9/w9_question3.cpp
@@ -0,0 +1,37 @@
+#include<vector>
+#include<list>
+#include<algorithm>
+#include<iostream>
+#include<queue>
+#define max 30
+using namespace std;
+
+int minComputation(int size, int files[])
+{
+	priority_queue<int, vector<int>, greater<int> > pq;
+	for (int i = 0; i < size; i++) {
+		pq.push(files[i]);
+	}
+	int count = 0;
+	while (pq.size() > 1) {
+		int first_smallest = pq.top();
+		pq.pop();
+		int second_smallest = pq.top();
+		pq.pop();
+		int temp = first_smallest + second_smallest;	
+		count += temp;
+		pq.push(temp);
+	}
+	return count;
+}
+int main()
+{
+	int n;
+	cin>>n;
+	int files[max]; 
+	for(int i=0;i<n;i++){
+		cin>>files[i];
+	}
+	cout << "Minimum Computations = "<< minComputation(n, files);
+	return 0;
+}
